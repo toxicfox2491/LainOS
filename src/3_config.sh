1ef90129aa4d40a281628a1710eab732ae6128c0
hasecilu <hasecilu@lainos.info>
2023-08-05 21:40:05 -0600

    Let new user belong to wheel group

--- src/3_config.sh
+++ src/3_config.sh
@@ -24,6 +24,11 @@ unzip ../assets/icons/StarLabsMiddleFingerMod.zip -d /usr/share/icons
 plymouth_hellonavi
 
 read -p "Enter the name of the new user: " user
-useradd --create-home --shell /bin/zsh "$user"
+useradd --create-home --groups wheel --shell /bin/zsh "$user"
 echo "Now type your new password"
 passwd "$user"
+
+echo "Don't forget to add the new user to the wheel group! Run:"
+echo -e "\tEDITOR=nvim visudo"
+echo "Uncomment this line:"
+echo -e "\t %wheel ALL=(ALL) ALL"
